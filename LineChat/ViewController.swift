d21e9b845b100187b347b35815a1dd9a0be9a4c5
alan <alan5899f@gmail.com>
2022-04-15 22:08:03 +0800

    Initial Commit

---
+++ LineChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LineChat
+//
+//  Created by 陳韋綸 on 2022/4/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
